25ee12a7e4ee12fb15892f7f70dd85f0284b6cd5
ItaSsa <ItaSsa>
2023-12-09 07:06:30 +0000

    Enverioment1- Include folder added
    There is an integration between the Databricks and an Account Storage-Delta Lake Gen 2, built on Azure Cloud. I mounted the containers in that Account Storage using by Service Principal Authentication and the configuration notebook has the path for these mounted paths.

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/formula1itassadl/raw'
+processed_folder_path='/mnt/formula1itassadl/processed'
+presentation_folder_path='/mnt/formula1itassadl/presentation'
